4559460165e34d07a7ff53c108030017455c85a9
Prashanth G K <prashkammar17@yahoo.com>
2020-04-28 19:40:04 +0530

    decide winner

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,77 +1,135 @@
 #include<iostream>
 using namespace std;
-char m[3][3]={'1','2','3','4','5','6','7','8','9'};
-char player='X';
+char m[3][3] = {'1','2','3','4','5','6','7','8','9'};
+char player = 'X';
 
 void table()
-    {
-        for(int i=0; i<3; i++)
+    {   
+        system("cls");
+        for(int i = 0; i < 3; i++)
         {
-            for(int j=0;j<3;j++)
+            for(int j = 0; j < 3; j++)
             {
-                cout<<m[i][j]<<" ";
+                cout<< m[i][j] <<" ";
             }
-            cout<<endl;
+            cout<< endl;
         }
     }
 void read()
     {
         char in;
-        cout<<"Press the number "<<endl;
+        cout<< "Press the number " <<endl;
         cin>>in;
         switch (in)
         {
-        case '1': m[0][0]=player;
+        case '1': m[0][0] = player;
                     break;
         
-        case '2': m[0][1]=player;
+        case '2': m[0][1] = player;
                     break;
 
-        case '3': m[0][2]=player;
+        case '3': m[0][2] = player;
                     break;
 
-        case '4': m[1][0]=player;
+        case '4': m[1][0] = player;
                     break;
 
-        case '5': m[1][1]=player;
+        case '5': m[1][1] = player;
                     break;
 
-        case '6': m[1][2]=player;
+        case '6': m[1][2] = player;
                     break;
 
-        case '7': m[2][0]=player;
+        case '7': m[2][0] = player;
                     break;
 
-        case '8': m[2][1]=player;
+        case '8': m[2][1] = player;
                     break;
 
-        case '9': m[2][2]=player;
+        case '9': m[2][2] = player;
                     break;
         
-        default: cout<<"Invalid input"<<endl;
+        default: cout<< "Invalid input" <<endl;
                 break;
         }
     }
 
 void switchplayer()
     {
-        if(player=='X')
-            player='O';
+        if(player  ==  'X')
+            player  =  'O';
 
         else
-            player='X';
+            player  =  'X';
     }
 
+char result()
+{   
+    // for player X
+    if (m[0][0]  ==  'X' && m[0][1]  ==  'X' && m[0][2]  ==  'X')
+        return 'X';
+
+    if (m[1][0]  ==  'X' && m[1][1]  ==  'X' && m[1][2]  ==  'X')
+        return 'X';
+
+    if (m[2][0]  ==  'X' && m[2][1]  ==  'X' && m[2][2]  ==  'X')
+        return 'X'; 
+
+    if (m[0][0]  ==  'X' && m[1][1]  ==  'X' && m[2][2]  ==  'X')
+        return 'X';
+    
+    if (m[2][0]  ==  'X' && m[1][1]  ==  'X' && m[1][2]  ==  'X')
+        return 'X';
+
+    //for player O
+
+    if (m[0][0]  ==  'O' && m[0][1]  ==  'O' && m[0][2]  ==  'O')
+        return 'O';
+
+    if (m[1][0]  ==  'O' && m[1][1]  ==  'O' && m[1][2]  ==  'O')
+        return 'O';
+
+    if (m[2][0]  ==  'O' && m[2][1]  ==  'O' && m[2][2]  ==  'O')
+        return 'O'; 
+
+    if (m[0][0]  ==  'O' && m[1][1]  ==  'O' && m[2][2]  ==  'O')
+        return 'O';
+    
+    if (m[2][0]  ==  'O' && m[1][1]  ==  'O' && m[1][2]  ==  'O')
+        return 'O';
+
+    // if draw
+    return 'D';
+
+
+}
 
 int main()
 {   
+    int count = 0;
     system("cls");
+    table();
     while(1)
     { 
-        table();
-        read();
+        count++;
+        read(); 
+        table();   
+        if (result() == 'X')
+        {
+            cout<< "Player X is the  winner!"<<endl;
+            break;
+        }
+        else if (result() == 'O')
+        {
+            cout<< "Player O is the winner !"<<endl;
+            break;
+        }
+        else if (result() == 'D' && count == 9)
+        {
+            cout<< " Tied! "<<endl;
+        }
         switchplayer();
-        system("cls");
+        
         
     }
 
